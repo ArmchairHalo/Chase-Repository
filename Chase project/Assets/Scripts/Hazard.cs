e29f79898ebb7022da2ad278c609a0f05e2cf8b0
ArmchairHalo <79139275+ArmchairHalo@users.noreply.github.com>
2021-04-23 20:57:19 +0100

    made it so the player character can die and that enemies can damage the player

---
+++ Chase project/Assets/Scripts/Hazard.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hazard : MonoBehaviour
+{
+    void OnCollisionEnter2D(Collision2D collisionData)
+    {
+        Collider2D objectWeCollidedWith = collisionData.collider;
+
+        PlayerHealth player = objectWeCollidedWith.GetComponent<PlayerHealth>();
+
+        if (player != null)
+        {
+            player.Kill();
+        }
+    }
+}
