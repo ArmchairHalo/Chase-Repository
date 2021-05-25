f7c851a202ffcd09b8af72116d3df1abdf4bcb0b
ArmchairHalo <79139275+ArmchairHalo@users.noreply.github.com>
2021-05-25 11:58:38 +0100

    added score to game

---
+++ Chase project/Assets/Scripts/Score.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Score : MonoBehaviour
+{
+    // Public variable
+    public Text scoreDisplay;
+
+    // private variables 
+    private int scoreValue = 0;
+
+    // Function to add to the players score
+    // not automatically called by unity - need to call it myself in my code 
+    public void AddScore(int toAdd)
+    {
+        // update the numerical value of the score (Action 1)
+        scoreValue = scoreValue + toAdd;
+
+        //update the display of the score based on the numerical 
+        scoreDisplay.text = scoreValue.ToString();
+    }
+}
---
+++ Chase project/Assets/Scripts/ScorePickup.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScorePickup : MonoBehaviour
+{
+    // Public Variable
+    // Editable in unity
+    // Useable by other scripts 
+    public int pickupValue = 1;
+
+    // Called by unity when this object overlaps with another object marked as a trigger
+    // this is the condition (if the player touches a collectable)
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        // check if the Score script is attached to the thing we bumped into 
+        Score scoreScript = other.GetComponent<Score>();
+
+        if (scoreScript != null)
+        {
+            // We have a scoreScript, so the thing we bumped into is the player!
+
+            //add the pickup value to the players current score (actions)
+            scoreScript.AddScore(pickupValue);
+
+            // we should then delete this object so we dont infinitely add score
+            Destroy(gameObject);
+        }
+    }
+
+}
