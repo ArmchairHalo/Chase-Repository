2b2721870cbb80fb150bb2a1a01339ff3b985713
ArmchairHalo <79139275+ArmchairHalo@users.noreply.github.com>
2021-05-27 10:45:16 +0100

    added script to go to the game over scene

--- Chase project/Assets/Scripts/PlayerHealth.cs
+++ Chase project/Assets/Scripts/PlayerHealth.cs
@@ -1,13 +1,18 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+
 
 public class PlayerHealth : MonoBehaviour
 {
+    public string gameOverScene;
+
     public void Kill()
     {
         Destroy(gameObject);
 
+        SceneManager.LoadScene(gameOverScene);
     }
    
 }
