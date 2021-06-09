61d598c42e6a3b9aa261f2820ae6edbe4e689b8f
ArmchairHalo <79139275+ArmchairHalo@users.noreply.github.com>
2021-06-09 10:55:41 +0100

    added level 1 to the game

--- Chase project/Assets/Scripts/Score.cs
+++ Chase project/Assets/Scripts/Score.cs
@@ -2,14 +2,18 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class Score : MonoBehaviour
 {
     // Public variable
     public Text scoreDisplay;
+    public int winningScore;
+    public string winningScene;
 
     // private variables 
     private int scoreValue = 0;
+
 
     // Function to add to the players score
     // not automatically called by unity - need to call it myself in my code 
@@ -20,5 +24,12 @@ public class Score : MonoBehaviour
 
         //update the display of the score based on the numerical 
         scoreDisplay.text = scoreValue.ToString();
+
+        // Check if the score is bigger than our min score 
+        if (scoreValue >= winningScore)
+        {
+            //our score is a winning score load win scene
+            SceneManager.LoadScene(winningScene);
+        }
     }
 }
